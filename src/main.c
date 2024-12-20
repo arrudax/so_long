03a8b1feac7272c8d5fe7e6c650c78e0156c6e7c
Marco Arruda <maanton2@student.42sp.org.br>
2024-12-20 14:44:32 -0300

    refactor: Move hooks to a higher level and fix close window functionality

--- src/main.c
+++ src/main.c
@@ -6,20 +6,23 @@
 /*   By: maanton2 <maanton2@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 17:02:38 by maanton2          #+#    #+#             */
-/*   Updated: 2024/12/18 19:59:29 by maanton2         ###   ########.org.br   */
+/*   Updated: 2024/12/20 14:05:19 by maanton2         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int32_t main(void)
+int	main(int argc, char **argv)
 {
 	t_game	game;
 
+	if (argc < 2)
+		return (1);
 	game.mlx = NULL;
-	game.window = NULL;
-	printf("%zu", ft_strlen("hello"));
-	ft_create_window(game.mlx);
+	game.render = ft_render_map(argv[1]);
+	ft_create_window(&game);
+	mlx_key_hook(game.mlx, (mlx_keyfunc)ft_close_window, &game);
+	mlx_loop(game.mlx);
     return (0);
 }
 
