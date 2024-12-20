f657a3eb83320881a2300bc2224747d2a670fdbf
Marco Arruda <maanton2@student.42sp.org.br>
2024-12-20 14:44:15 -0300

    feat: Update game struct to support new render feature

--- src/window/window.h
+++ src/window/window.h
@@ -6,7 +6,7 @@
 /*   By: maanton2 <maanton2@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 16:42:22 by maanton2          #+#    #+#             */
-/*   Updated: 2024/12/18 19:59:59 by maanton2         ###   ########.org.br   */
+/*   Updated: 2024/12/20 11:40:14 by maanton2         ###   ########.org.br   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,15 @@
 # include <stdlib.h>
 # include "MLX42/MLX42.h"
 
-void	ft_create_window(mlx_t *mlx);
-void	ft_close_window(void *param);
+typedef struct s_render t_render;
+
+typedef struct s_game 
+{
+	mlx_t		*mlx;
+	t_render	*render;
+} t_game;
+
+void	ft_create_window(t_game *game);
+void	ft_close_window(mlx_key_data_t key, t_game *game);
 
 #endif
